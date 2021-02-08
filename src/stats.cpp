a8da9cf2b471baa6cd7d8f6ae28956b920ffcb34
cecilia <lsscecilia8888@gmail.com>
2021-02-08 13:36:32 +0800

    optimise code

--- src/stats.cpp
+++ src/stats.cpp
@@ -13,12 +13,12 @@ struct Sequence {  // or any other name
   string quality; 
   public:
   Sequence(  // required arguments
-      const char* name, std::uint32_t name_len,
-      const char* data, std::uint32_t data_len ,
-      const char* quality, std::uint32_t quality_len) :
-        name(name, name_len), 
-        data(data, data_len), 
-        quality(data, quality_len){}
+    const char* name, std::uint32_t name_len,
+    const char* data, std::uint32_t data_len ,
+    const char* quality, std::uint32_t quality_len) :
+      name(name, name_len), 
+      data(data, data_len), 
+      quality(data, quality_len){}
 }; 
 
 static struct option long_options[] = {
@@ -38,54 +38,44 @@ void Help(){
 }; 
 
 int main(int argc, char **argv){
-    int option_index = 0;
-	int c=0; 
-    while ((c = getopt_long (argc, argv, "vh",
-				   long_options, &option_index)) != -1){
-	
-		switch (c) {
-			case 'v':
-			  ProjectVersion(); 
-			  break;
+  int option_index = 0;
+  int c=0; 
+  while ((c = getopt_long (argc, argv, "vh", long_options, &option_index)) != -1){
+  switch (c) {
+    case 'v':
+      ProjectVersion(); 
+      break;
 
-			case 'h':
-			  Help(); 
-			  break;
-			case '?':
-			  /* getopt_long already printed an error message. */
-			  break;
+    case 'h':
+      Help(); 
+      break;
+    case '?':
+      /* getopt_long already printed an error message. */
+      break;
 
-			default:
-			  //abort();
-			  break;	
-		}
-	}
+    default:
+      //abort();
+      break;	
+  }
+}
 	
 	/* Print any remaining command line arguments (not options). */
-	if (optind < argc){	
-        auto p = bioparser::Parser<Sequence>::Create<bioparser::FastqParser>(argv[optind]);
-        // parse in chunks
-        std::vector<std::unique_ptr<Sequence>> s;
-        while (true) {
-        auto c = p->Parse(1ULL << 30);  // 1 GB
-        if (c.empty()) {
-            break;
-        }
-        s.insert(
-            s.end(),
-            std::make_move_iterator(c.begin()),
-            std::make_move_iterator(c.end()));
-        }
-
-        //get sum of reads
-        int sum=0; 
-        for (int i=0; i<s.size(); i++){
-            sum += s[i]->data.length(); 
-        }   
-
-        cout << "Number of reads: " << s.size() << endl; 
-        cout << "sum of all reads: " << sum << endl;  ; 
-        cout << "Average length of reads: " << sum/s.size() << endl; 
+if (optind < argc){	
+  auto p = bioparser::Parser<Sequence>::Create<bioparser::FastqParser>(argv[optind]);
+  // parse in chunks
+  std::vector<std::unique_ptr<Sequence>> s;
+  std::size_t sum = 0, num = 0;
+  while (true) {
+    auto c = p->Parse(1ULL << 30);  // 1 GB
+    if (c.empty()) {
+      break;
     }
+    num += c.size();
+    for (const auto& it : c) {
+      sum += it->data.size();
+    }
+  }
+  std::cerr << sum / static_cast<double>(num) << std::endl;
+  }
 
 }
